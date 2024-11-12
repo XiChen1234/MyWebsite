c61a2fcc8e8465129afa57b5d640a1aab9401cec
Xi_Chen <xichen888.private@gmail.com>
2024-11-13 01:00:09 +0800

    func(html,css,js): update sidebar
    为sidebar的样式和功能进行更新，能够平滑回到顶部

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/*
+ * 侧边栏点击回到顶部功能
+ */
+const sidebar = document.getElementById('sidebar')
+
+sidebar.addEventListener('click', function() {
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+})
