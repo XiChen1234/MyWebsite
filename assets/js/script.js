02d5dfea21aeaded0dd96228ac43bd53e6de7059
Xi_Chen <xichen888.private@gmail.com>
2024-11-13 20:33:06 +0800

    func: add topbar to responsive
    将顶部导航栏修改为响应式
    并且完成了article的排布

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const sidebar = document.getElementById('sidebar')
+const side_list = document.getElementById('side-list')
 
 /**
  * 明暗切换（暂时绑定到sidebar中）
@@ -9,3 +10,25 @@ const body = document.body;
 sidebar.addEventListener('click', function() {
     body.classList.toggle('dark-mode')
 })
+
+/**
+ * 展开侧边栏，打开全局遮罩
+ */
+const menu = document.getElementById('menu')
+const overlay = document.getElementById('overlay')
+
+side_list.addEventListener('click', changeSideList)
+
+/**
+ * 在侧边栏中点击按钮跳转，关闭
+ */
+menu.addEventListener('click', changeSideList)
+
+
+/**
+ * 切换侧栏状态
+ */
+function changeSideList() {
+    menu.classList.toggle('active')
+    overlay.classList.toggle('active')
+}
