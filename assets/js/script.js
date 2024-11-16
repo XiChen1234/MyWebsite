0b5df4e2af6e4c588d14daf32c6a6ae122175d45
XiChen1234 <xichen888.private@gmail.com>
2024-11-16 11:00:48 +0800

    fix: make navbar animation better
    优化navbar的动画效果，平滑展开、平滑关闭

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,31 @@ light.addEventListener('click', function () {
 /**
  * 展开侧边栏，打开全局遮罩
  */
-const side_list = document.getElementById('side-list')
-const menu = document.getElementById('menu')
+
+// /**
+//  * 在侧边栏中点击按钮跳转，关闭
+//  */
+// menu.addEventListener('click', changeSideList)
+
+
+
+const sideList = document.getElementById('side-list');
+const menu = document.getElementById('menu');
 const overlay = document.getElementById('overlay')
+let isOpen = false;
 
-side_list.addEventListener('click', changeSideList)
-
-/**
- * 在侧边栏中点击按钮跳转，关闭
- */
+sideList.addEventListener('click', changeSideList)
 menu.addEventListener('click', changeSideList)
-
 
 /**
  * 切换侧栏状态
  */
 function changeSideList() {
-    menu.classList.toggle('active')
-    overlay.classList.toggle('active')
+    overlay.classList.toggle('active') // 全局遮罩
+    if (isOpen) {
+        menu.style.maxHeight = null;
+    } else {
+        menu.style.maxHeight = menu.scrollHeight + 'px';
+    }
+    isOpen = !isOpen;
 }
