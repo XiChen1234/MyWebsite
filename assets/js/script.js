b4ba9ca9913bb8e6eb02d6cf06a92308601ce953
XiChen1234 <xichen888.private@gmail.com>
2024-11-26 01:29:13 +0800

    refactor: spilt script.js
    拆分script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,6 @@ light.addEventListener('click', function () {
 /**
  * 展开侧边栏，打开全局遮罩
  */
-
-// /**
-//  * 在侧边栏中点击按钮跳转，关闭
-//  */
-// menu.addEventListener('click', changeSideList)
-
-
-
 const sideList = document.getElementById('side-list');
 const menu = document.getElementById('menu');
 const overlay = document.getElementById('overlay')
@@ -41,22 +33,3 @@ function changeSideList() {
     }
     isMenuOpen = !isMenuOpen;
 }
-
-/**
- * 展开技能list，打开全局遮罩
- */
-const openSkillList = document.querySelectorAll('article#skill .content .skill-content .skill-header .iconfont.icon-right')
-const skillList = document.querySelectorAll('article#skill .content .skill-content .skill-desc')
-const isSkillOpen = [false, false, false]
-
-openSkillList.forEach(function(openSkill, index) {
-    openSkill.addEventListener('click', function() {
-        if (isSkillOpen[index]) {
-            skillList[index].style.maxHeight = null;
-        } else {
-            skillList[index].style.maxHeight = skillList[index].scrollHeight + 'px';
-        }
-        openSkill.classList.toggle('active')
-        isSkillOpen[index] = !isSkillOpen[index]
-    })
-})
---
+++ assets/js/skill.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * 展开技能list
+ */
+const openSkillList = document.querySelectorAll('article#skill .content .skill-content .skill-header .iconfont.icon-right')
+const skillList = document.querySelectorAll('article#skill .content .skill-content .skill-desc')
+const isSkillOpen = [false, false, false]
+
+openSkillList.forEach(function(openSkill, index) {
+    openSkill.addEventListener('click', function() {
+        if (isSkillOpen[index]) {
+            skillList[index].style.maxHeight = null;
+        } else {
+            skillList[index].style.maxHeight = skillList[index].scrollHeight + 'px';
+        }
+        openSkill.classList.toggle('active')
+        isSkillOpen[index] = !isSkillOpen[index]
+    })
+})
