020c13d585211560100779b7d938e304e7123a00
XiChen1234 <xichen888.private@gmail.com>
2025-09-25 23:36:07 +0800

    feat: change icon from ali-icon to local icon
    将icon从阿里iconfont修改到本地icon

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import './style/reset.css'
 import './style/color.css'
 import './style/common.css'
 
+// icon
+import './assets/icons/iconfont.css'
+import './assets/color/iconfont.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
