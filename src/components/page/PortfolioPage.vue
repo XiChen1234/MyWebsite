ee2d09bb5eca5b10004fe5d100ac1c142fe332b2
XiChen1234 <xichen888.private@gmail.com>
2026-01-20 14:20:50 +0800

    feat: update paws up project
    更新paws up到项目中
    更新其他项目索引

--- src/components/page/PortfolioPage.vue
+++ src/components/page/PortfolioPage.vue
@@ -5,7 +5,7 @@ import MyWebsite from '@/assets/img/MyWebsite.png';
 import NoBG from '@/assets/img/NoBG.png';
 import Werewolf from '@/assets/img/狼人杀.png';
 import Snake from '@/assets/img/贪吃蛇.png';
-import SmartChoose from '@/assets/img/慧选3C.png';
+import pawsUp from '@/assets/img/paws_up.png';
 
 
 /**
@@ -34,6 +34,15 @@ const data: PortfolioItem[] = [
   },
   {
     id: 'project2',
+    title: 'Paws Up! - 节奏音游',
+    description: '使用Godot引擎独立开发的单音轨节奏音游，包含五位性格各异的NPC，每个都有独特的音乐和打击方式。',
+    imagePath: pawsUp,
+    tags: ['Godot Engine', 'GDScript', 'WebGL', '游戏设计'],
+    link: 'https://github.com/XiChen1234/DemoJam',
+    demo: 'https://xichen1234.itch.io/paws-up',
+  },
+  {
+    id: 'project3',
     title: 'NoBG',
     description: '一个简单、快速、隐私保护的在线图片背景去除工具，完全在浏览器中本地处理，无需上传任何图片到服务器。',
     imagePath: NoBG,
@@ -42,7 +51,7 @@ const data: PortfolioItem[] = [
     demo: 'https://xichen8.top/NoBG',
   },
   {
-    id: 'project3',
+    id: 'project4',
     title: '狼人杀统计模拟器',
     description: '设计并实现一个7人局狼人杀游戏模拟器，通过为不同角色设置不同的AI策略，完全自动运行多轮游戏并统计不同策略组合下的各阵营胜率。该系统无需任何真人参与或输入，完全通过AI策略自主模拟游戏进程。 模拟器支持保存每单局对局的游戏进程信息，供系统主查看复盘',
     imagePath: Werewolf,
@@ -50,19 +59,12 @@ const data: PortfolioItem[] = [
     link: 'https://github.com/XiChen1234/WolfGame'
   },
   {
-    id: 'project4',
+    id: 'project5',
     title: '贪吃蛇小游戏',
     description: '基于Qt库的C++基础贪吃蛇游戏项目，玩家通过控制蛇的移动来吃到食物，同时避免撞到自己或边界。',
     imagePath: Snake,
     tags: ['Qt', 'C++', '游戏开发'],
     link: 'https://github.com/XiChen1234/SnakeGame'
-  },
-  {
-    id: 'project5',
-    title: '慧选3C-AIGC数码导购',
-    description: '引入AIGC解决数码选购难题，提供个性化推荐服务的平台。',
-    imagePath: SmartChoose,
-    tags: ['Java', 'Spring Boot', 'MySQL', 'Vue.js', 'AIGC算法']
   }
 ];
 </script>
